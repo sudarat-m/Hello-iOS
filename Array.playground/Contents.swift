75ff9b0ed3fe45dacbc66b8354ceb1b3da1f18e0
sudarat <muayka2000@yahoo.com>
2017-09-09 14:37:29 +0700

    Array

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Array Format
+
+var numberAInts = [11,22,33,44,55]
+
+print("ขนาดของ Array\(numberAInts.count)")
+
+print("numberAints[2]==>\(numberAInts[2])")
+
+print("numberAints[4]==>\(numberAInts[4])")
+
+print("numberAints[4]==>\(numberAInts[1])")
+
+//add member
+numberAInts.append(66)
+numberAInts
+
+
+//delete member
+numberAInts.remove(at: 1)
+
+
+
+
+
+
+
+
+
