1b39fa6296b98170bacb7b9ab3d2a98aa049e057
sudarat <muayka2000@yahoo.com>
2017-09-09 11:38:33 +0700

    test

--- Hello iOS/Hello iOS/ViewController.swift
+++ Hello iOS/Hello iOS/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    //Implicit 
+    var nameString = ""
+    
+    
+    
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    
+    @IBAction func addButton(_ sender: Any) {
+        
+        nameString = nameTextField.text!
+        print("You type ==>\(nameString)")
+        
+        
+        
+    }// addButton
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }   // Main function
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    } //Manage Memory
 
 
-}
+}// Main Class
 
