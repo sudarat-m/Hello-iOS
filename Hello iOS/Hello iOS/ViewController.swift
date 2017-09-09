18c8f4773e83e4eda3ff776788453464b637d613
sudarat <muayka2000@yahoo.com>
2017-09-09 11:02:06 +0700

    test

---
+++ Hello iOS/Hello iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello iOS
+//
+//  Created by Sudarat on 9/9/2560 BE.
+//  Copyright © 2560 Sudarat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
