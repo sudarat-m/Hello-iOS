d57ad13eac1b0789250ef3e38d270490dd5a4b15
sudarat <muayka2000@yahoo.com>
2017-09-09 10:19:31 +0700

    test

---
+++ Variable and Constant.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//การประกาศตัวแปรบนภาษา Swift
+//Implicit 
+
+//ตัวแปรถ้าเป็น var จะสามาถทำการแก้ไขค่าได้
+var nameString = "Sudarat"
+
+nameString="Saramoon"
+
+//Constant
+//ตัวแปร let เป็นค่าคงที่ไม่สามารถแก้ไขได้
+
+let surnameString = "ABCD"
+//surnameString="EGHG" //let เป็นค่าคงที่ไม่สามารถแก้ไขได้
+
+
+
